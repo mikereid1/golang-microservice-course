071622d4083477bc352e6ae5f6ab7dd08f9d373b
mike <michaelreid87@protonmail.com>
2022-08-12 20:05:10 +0200

    late initial commit

---
+++ authentication-service/cmd/api/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"authentication/data"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "8080"
+
+type Config struct {
+	DB     *sql.DB
+	Models data.Models
+}
+
+func main() {
+	log.Printf("Starting authentication service")
+
+	// TODO connect to db
+
+	// set up config
+	app := Config{}
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+
+}
---
+++ broker-service/cmd/api/handlers.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"net/http"
+)
+
+func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
+	payload := jsonResponse{
+		Error:   false,
+		Message: "Hit the broker",
+	}
+
+	_ = app.writeSON(w, http.StatusOK, payload)
+}
