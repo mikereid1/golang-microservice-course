1646d5a1c796bfe4e3fa701f2a4b4d6ed08b4151
mike <michaelreid87@protonmail.com>
2022-08-13 23:49:04 +0200

    add routes and start server

--- mail-service/cmd/api/main.go
+++ mail-service/cmd/api/main.go
@@ -1,8 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
 type Config struct {
 }
 
-func main() {
+const webPort = "80"
 
+func main() {
+	app := Config{}
+
+	log.Println("Starting mail service on port", webPort)
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
 }
