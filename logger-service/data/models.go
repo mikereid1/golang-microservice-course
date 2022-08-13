f2b9a9c250620c1cdbb2cacc5bf242a6a53cd58b
mike <michaelreid87@protonmail.com>
2022-08-13 14:51:33 +0200

    add models structs

---
+++ logger-service/data/models.go
@@ -0,0 +1,28 @@
+package data
+
+import (
+	"go.mongodb.org/mongo-driver/mongo"
+	"time"
+)
+
+var client *mongo.Client
+
+func New(mongo *mongo.Client) Models {
+	client = mongo
+
+	return Models{
+		LogEntry: LogEntry{},
+	}
+}
+
+type Models struct {
+	LogEntry LogEntry
+}
+
+type LogEntry struct {
+	ID        string    `bson:"_id,omitempty" json:"id,omitempty"`
+	Name      string    `bson:"name" json:"names"`
+	Data      string    `bson:"data" json:"data"`
+	CreatedAt time.Time `bson:"created_at" json:"created_at"`
+	UpdatedAt time.Time `bson:"updated_at" json:"updated_at"`
+}
