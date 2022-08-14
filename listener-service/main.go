6ceb29565ee9243662d7a0392ff221e9771d9968
mike <michaelreid87@protonmail.com>
2022-08-14 13:32:27 +0200

    add stub for listener service

--- listener-service/main.go
+++ listener-service/main.go
@@ -1,1 +1,13 @@
-package listener_service
+package main
+
+func main() {
+	// connect to rabbitmq
+
+	// start listening for messages
+
+	// create consumer
+
+	// watch the queue and consume events
+
+	// do something with it
+}
