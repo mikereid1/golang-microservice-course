f385f479c90dbc53bd1173bfc5865e2156bb2077
mike <michaelreid87@protonmail.com>
2022-08-15 22:24:17 +0200

    user correct rabbitmq url

--- listener-service/main.go
+++ listener-service/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	amqp "github.com/rabbitmq/amqp091-go"
+	"listener/event"
 	"log"
 	"math"
 	"os"
@@ -20,12 +21,20 @@ func main() {
 	log.Println("Connected to RabbitMQ")
 
 	// start listening for messages
+	log.Println("Listening for RabbitMQ messages...")
 
 	// create consumer
 
-	// watch the queue and consume events
+	consumer, err := event.NewConsumer(conn)
+	if err != nil {
+		panic(err)
+	}
 
-	// do something with it
+	// watch the queue and consume events
+	err = consumer.Listen([]string{"log.INFO", "log.WARNING", "log.ERROR"})
+	if err != nil {
+		log.Println(err)
+	}
 }
 
 func connect() (*amqp.Connection, error) {
@@ -34,11 +43,12 @@ func connect() (*amqp.Connection, error) {
 	var connection *amqp.Connection
 
 	for {
-		c, err := amqp.Dial("amqp://guest:guest@localhost")
+		c, err := amqp.Dial("amqp://guest:guest@rabbitmq")
 		if err != nil {
 			fmt.Println("RabbitMQ not yet ready")
 			count++
 		} else {
+			fmt.Println("Connected to RabbitMQ")
 			connection = c
 			break
 		}
